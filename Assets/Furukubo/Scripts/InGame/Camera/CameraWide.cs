6da9e41c89eec41765eba7f39206e1d4879c1ebf
MSI\Tsume <auioue929@gmail.com>
2026-06-17 18:34:04 +0900

    神原の本番シーンでもカメラが動くように修正しました。志望演出等はまだ動かないですつなげられてないので。

--- Assets/Furukubo/Scripts/InGame/Camera/CameraWide.cs
+++ Assets/Furukubo/Scripts/InGame/Camera/CameraWide.cs
@@ -8,6 +8,9 @@ namespace InGame.Camera
     public class CameraWide : MonoBehaviour
     {
         [Header("Parameters")]
+        [SerializeField] private string _playerNorthTag;
+        [SerializeField] private string _playerSouthTag;
+        [SerializeField] private string _bodyObjName;
         [SerializeField] private float _wideMin;
         [SerializeField] private float _wideMax;
         [SerializeField] private float _widePadding;
@@ -15,40 +18,42 @@ namespace InGame.Camera
 
         [Header("References")]
         [SerializeField] private UnityEngine.Camera _cam;
-        [SerializeField] private Transform _tr;
-        [SerializeField] private Transform _targetATr;
-        [SerializeField] private Transform _targetBTr;
         [SerializeField] private Transform _targetBGPositionTr;
         [SerializeField] private Transform _targetBGScaleTr;
 
+        private Transform _playerNorthTr;
+        private Transform _playerSouthTr;
         private const float _sizeX = 26f;
         private const float _sizeY = 15f;
 
-        private Vector2 PointA
+        private Vector2 PlayerNorthPos
         {
             get
             {
-                if (_targetATr == null) return Vector2.zero;
-                return _targetATr.position;
+                if (_playerNorthTr == null) return Vector2.zero;
+                return _playerNorthTr.position;
             }
         }
 
-        private Vector2 PointB
+        private Vector2 PlayerSouthPos
         {
             get
             {
-                if(_targetBTr == null) return Vector2.zero;
-                return _targetBTr.position;
+                if(_playerSouthTr == null) return Vector2.zero;
+                return _playerSouthTr.position;
             }
         }
 
         private void Update()
         {
-            Vector2 centerPoint = (PointA + PointB) * 0.5f;
+            if(_playerNorthTr == null) SetPlayerNorthTr(GameObject.FindGameObjectWithTag(_playerNorthTag));
+            if (_playerSouthTr == null) SetPlayerSouthTr(GameObject.FindGameObjectWithTag(_playerSouthTag));
 
-            float wide = Mathf.Clamp((PointA - PointB).magnitude * 0.5f, _wideMin, _wideMax) + _widePadding;
+            Vector2 centerPoint = (PlayerNorthPos + PlayerSouthPos) * 0.5f;
 
-            if(_tr != null) _tr.position = centerPoint;
+            float wide = Mathf.Clamp((PlayerNorthPos - PlayerSouthPos).magnitude * 0.5f, _wideMin, _wideMax) + _widePadding;
+
+            transform.position = centerPoint;
             if (_cam != null) _cam.orthographicSize = wide;
 
             if (_targetBGPositionTr == null || _targetBGScaleTr == null) return;
@@ -61,6 +66,44 @@ namespace InGame.Camera
 
             _targetBGPositionTr.localPosition = new Vector2(-bgOffsetX, -bgOffsetY);
             _targetBGScaleTr.localScale = new Vector3(bgScale, bgScale, 1f);
+        }
+
+        private void SetPlayerNorthTr(GameObject obj)
+        {
+            if (obj == null) return;
+
+            if (_playerNorthTr == null)
+            {
+                Transform[] trs = obj.GetComponentsInChildren<Transform>();
+                foreach (Transform t in trs)
+                {
+                    if (t.gameObject.name == _bodyObjName)
+                    {
+                        _playerNorthTr = t;
+                        Debug.Log("Player North Finded!");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void SetPlayerSouthTr(GameObject obj)
+        {
+            if (obj == null) return;
+
+            if (_playerSouthTr == null)
+            {
+                Transform[] trs = obj.GetComponentsInChildren<Transform>();
+                foreach (Transform t in trs)
+                {
+                    if (t.gameObject.name == _bodyObjName)
+                    {
+                        _playerSouthTr = t;
+                        Debug.Log("Player South Finded!");
+                        return;
+                    }
+                }
+            }
         }
     }
 }
