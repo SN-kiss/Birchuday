2c4f19056b9757f689e84af02a25356f30910acb
GOD <a-kambara4170@st.kyoto-art.ac.jp>
2026-08-17 22:45:04 +0900

    ハードモード12できました仮。

---
+++ Assets/GOD/Script/GOD_GoHard.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GOD_GoHard : MonoBehaviour
+{
+    public void OnGameStart()
+    {
+        SceneManager.LoadScene("Hard_Link");
+    }
+}
